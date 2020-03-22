2b1a1af1362da4dad2a5e99108bfb8bc2b397312
Pieter De Praetere <pieter.de.praetere@helptux.be>
2020-03-22 19:06:23 +0100

    Not great yet

--- manifests/config/letsencrypt/cert.pp
+++ manifests/config/letsencrypt/cert.pp
@@ -1,17 +1,19 @@
 ##
 # Generate a letsencrypt certificate
 define hx_website::config::letsencrypt::cert (
-  Array $domains   = [$::fqdn],
-  String $provider = 'dns_cf'
+  Array $domains      = [$::fqdn],
+  String $provider    = 'dns_cf',
+  Hash $provider_auth = undef,
 ) {
 
   $_domains = join($domains, ' -d ')
 
   exec {"generate-certificate-${domains[0]}":
-    command => "/bin/bash -c 'source /var/opt/app/letsencrypt/.providers/.${provider} && /var/opt/app/letsencrypt/acme/acme.sh --issue --dns ${provider} -d ${_domains}'",
-    cwd     => '/var/opt/app/letsencrypt/acme',
-    user    => 'letsencrypt',
-    creates => "/var/opt/app/letsencrypt/.acme.sh/${domains[0]}/${domains[0]}.cer"
+    command     => "/var/opt/app/letsencrypt/acme/acme.sh --issue --dns ${provider} -d ${_domains}",
+    cwd         => '/var/opt/app/letsencrypt/acme',
+    user        => 'letsencrypt',
+    creates     => "/var/opt/app/letsencrypt/.acme.sh/${domains[0]}/${domains[0]}.cer",
+    environment => $provider_auth
   }
 
 }
