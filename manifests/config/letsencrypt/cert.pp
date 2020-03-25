7201c9d53f0c8c924981aa0c14df6fe22ad7c97f
Pieter De Praetere <pieter.de.praetere@helptux.be>
2020-03-25 22:07:45 +0100

    Missing arg

--- manifests/config/letsencrypt/cert.pp
+++ manifests/config/letsencrypt/cert.pp
@@ -2,18 +2,16 @@
 # Generate a letsencrypt certificate
 define hx_website::config::letsencrypt::cert (
   Array $domains      = [$::fqdn],
-  String $provider    = 'dns_cf',
-  Hash $provider_auth = undef,
+  String $provider    = 'dns_cf'
 ) {
 
   $_domains = join($domains, ' -d ')
 
   exec {"generate-certificate-${domains[0]}":
-    command     => "/var/opt/app/letsencrypt/acme/acme.sh --issue --dns ${provider} -d ${_domains}",
-    cwd         => '/var/opt/app/letsencrypt/acme',
-    user        => 'letsencrypt',
-    creates     => "/var/opt/app/letsencrypt/.acme.sh/${domains[0]}/${domains[0]}.cer",
-    environment => $provider_auth
+    command => "/var/opt/app/letsencrypt/acme/acme.sh --issue --dns ${provider} -d ${_domains}",
+    cwd     => '/var/opt/app/letsencrypt/acme',
+    user    => 'letsencrypt',
+    creates => "/var/opt/app/letsencrypt/.acme.sh/${domains[0]}/${domains[0]}.cer"
   }
 
 }
